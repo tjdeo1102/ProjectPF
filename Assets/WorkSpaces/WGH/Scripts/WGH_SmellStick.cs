0a83bf9bf68c1eaff5857e12dd11ede60a81c222
pang <eys555@naver.com>
2024-12-27 15:26:07 +0900

    [Feat] 시향노트 시향지 이펙트 기능 + 상호작용 조건 추가

--- Assets/WorkSpaces/WGH/Scripts/WGH_SmellStick.cs
+++ Assets/WorkSpaces/WGH/Scripts/WGH_SmellStick.cs
@@ -9,55 +9,36 @@ using Unity.VisualScripting;
 public class WGH_SmellStick : MonoBehaviourPun
 {
     [SerializeField] private GameObject customer;
-    [SerializeField] private float interactionDist;
+    [SerializeField] private float interactionDist;     // 상호작용 거리
+    [SerializeField] private ParticleSystem aura;
     public E_WGH_NoteType NoteType;
     public event Action OnBestInteract;
     public event Action OnLikeInteract;
     public event Action OnQuestionInteract;
     public event Action OnDespairInteract;
 
-    private float interactTime;
-    [SerializeField] private float needTime;
+    private float curTime;                              // 현재 시간
+    [SerializeField] private float needTime;            // 시향에 필요한 시간
+    private bool isAbsorbed;
 
     private Coroutine timeRoutine;
     public void Interact()
     {
         StartCoroutine(InteractRoutine());
     }
-    
-    IEnumerator InteractRoutine()
-    {
-        yield return new WaitForSeconds(1);
-        if (Vector3.Distance(transform.position, customer.transform.position) < interactionDist)
-        {
-            Debug.Log("상호작용");
-            if(NoteType == customer.GetComponent<WGH_NPCController>().BestMaterial)
-            {
-                OnBestInteract?.Invoke();
-            }
-            else if(NoteType == customer.GetComponent<WGH_NPCController>().LikeMaterial || NoteType == customer.GetComponent<WGH_NPCController>().LikeMaterial2)
-            {
-                OnLikeInteract?.Invoke();
-            }
-            else if (NoteType == customer.GetComponent<WGH_NPCController>().QuestionMaterial || NoteType == customer.GetComponent<WGH_NPCController>().QuestionMaterial2)
-            {
-                OnQuestionInteract?.Invoke();
-            }
-            else
-            {
-                OnDespairInteract?.Invoke();
-            }
-        }
-        yield break;
-    }
-    
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.TryGetComponent(out WGH_InteractArea interactArea))
+        if(other.gameObject.TryGetComponent(out WGH_InteractionNote testNote) && NoteType == testNote.NoteType)
+        {
+            Debug.Log("이펙트");
+            aura.gameObject.SetActive(true);
+            isAbsorbed = true;
+        }
+        if (other.gameObject.TryGetComponent(out WGH_InteractArea interactArea) && isAbsorbed == true)
         {
             customer = interactArea.GetComponentInParent<WGH_NPCController>().gameObject;
-            interactTime = 0f;
+            curTime = 0f;
             timeRoutine = StartCoroutine(TimeRoutine());
         }
     }
@@ -79,15 +60,43 @@ public class WGH_SmellStick : MonoBehaviourPun
     {
         while (true)
         {
-            Debug.Log(interactTime);
-            interactTime += Time.deltaTime;
-            if(interactTime >= needTime)
+            Debug.Log(curTime);
+            curTime += Time.deltaTime;
+            if(curTime >= needTime)
             {
                 StartCoroutine(InteractRoutine());
-                interactTime = 0f;
+                curTime = 0f;
+                aura.gameObject.SetActive(false);
+                isAbsorbed = false;
                 yield break;
             }
             yield return null;
         }
+    }
+
+    IEnumerator InteractRoutine()
+    {
+        yield return new WaitForSeconds(1);
+        if (Vector3.Distance(transform.position, customer.transform.position) < interactionDist)
+        {
+            Debug.Log("상호작용");
+            if (NoteType == customer.GetComponent<WGH_NPCController>().BestMaterial)
+            {
+                OnBestInteract?.Invoke();
+            }
+            else if (NoteType == customer.GetComponent<WGH_NPCController>().LikeMaterial || NoteType == customer.GetComponent<WGH_NPCController>().LikeMaterial2)
+            {
+                OnLikeInteract?.Invoke();
+            }
+            else if (NoteType == customer.GetComponent<WGH_NPCController>().QuestionMaterial || NoteType == customer.GetComponent<WGH_NPCController>().QuestionMaterial2)
+            {
+                OnQuestionInteract?.Invoke();
+            }
+            else
+            {
+                OnDespairInteract?.Invoke();
+            }
+        }
+        yield break;
     }
 }
