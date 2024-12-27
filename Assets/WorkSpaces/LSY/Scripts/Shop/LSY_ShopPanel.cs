5e2f16e4b6272804a25c412adfc54584497eb883
leesiyeon99 <qsqs9090@naver.com>
2024-12-27 21:15:57 +0900

    [Feat] Shop 씬 구성
    #11

---
+++ Assets/WorkSpaces/LSY/Scripts/Shop/LSY_ShopPanel.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LSY_ShopPanel : MonoBehaviour
+{
+    public enum Panel { Information, Sound, Control }
+    [SerializeField] GameObject optionPanel;
+    [SerializeField] GameObject informationPanel;
+    [SerializeField] GameObject soundPanel;
+    [SerializeField] GameObject controlPanel;
+
+    [SerializeField] Button informationButton;
+    [SerializeField] Button soundButton;
+    [SerializeField] Button controlButton;
+
+    private void Start()
+    {
+        informationButton.onClick.AddListener(InformationButton);
+        soundButton.onClick.AddListener(SoundButton);
+        controlButton.onClick.AddListener(ControlButton);
+    }
+
+    private void SetActivePanel(Panel panel)
+    {
+        informationPanel.SetActive(panel == Panel.Information);
+        soundPanel.SetActive(panel == Panel.Sound);
+        controlPanel.SetActive(panel == Panel.Control);
+    }
+
+    public void CloseButton()
+    {
+        SetActivePanel(Panel.Information);
+        optionPanel.SetActive(false);
+    }
+
+    public void InformationButton()
+    {
+        SetActivePanel(Panel.Information);
+    }
+
+    public void SoundButton()
+    {
+        SetActivePanel(Panel.Sound);
+    }
+
+    public void ControlButton()
+    {
+        SetActivePanel(Panel.Control);
+    }
+}
