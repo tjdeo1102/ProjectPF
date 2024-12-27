94c632e79e47cd275ee5321b049a29d005cb7ea6
tjdeo1102 <57614322+tjdeo1102@users.noreply.github.com>
2024-12-27 18:08:32 +0900

    [Feat] 프로토타입 가마솥 구현

--- Assets/WorkSpaces/KSD/Scripts/TestScripts/Objects/KSD_NetworkGrabInteractable.cs
+++ Assets/WorkSpaces/KSD/Scripts/TestScripts/Objects/KSD_NetworkGrabInteractable.cs
@@ -19,8 +19,9 @@ public class KSD_NetworkGrabInteractable : XRGrabInteractable
         {
             base.OnSelectEntered(args);
 
-            interactable.RequestOwnership();
-            interactable.RPC("ChangeRigidbodySetting", RpcTarget.All, true);
+            interactable.TransferOwnership(PhotonNetwork.LocalPlayer);
+            print("소유권 양도");
+            interactable.RPC("ChangeRigidbodySetting", RpcTarget.AllViaServer, true);
         }
     }
 
@@ -40,62 +41,13 @@ public class KSD_NetworkGrabInteractable : XRGrabInteractable
     }
 
     [PunRPC]
-    public void ChangeRigidbodySetting(bool isSelect)
+    public void ChangeRigidbodySetting(bool isSelect, PhotonMessageInfo info)
     {
-        var rigid = GetComponent<Rigidbody>();
-        rigid.isKinematic = isSelect;
         isGrabInNetwork = isSelect;
 
-        //var view = GetComponent<PhotonView>();
-        //// 본인이 아니라면 상용작용 못하는 물체로 만들기
-        //if (isSelect
-        //    && view.IsMine == false)
-        //{
-
-        //}
-        //else
-        //{
-        //    // 물체를 놓을 때는 상호작용 상태 활성화
-        //    intera
-        //}
-
+        // 잡은 경우에, 잡은 사람 빼고, 물리 비활성화
+        if (info.Sender.IsLocal) return;
+        var rigid = GetComponent<Rigidbody>();
+        rigid.isKinematic = isSelect;     
     }
-
-    //[PunRPC]
-    //public void SelectControlRPC(bool isSelect, PhotonMessageInfo info)
-    //{
-    //    // 1. 잡으라는 RPC가 날라온 경우, 해당 RPC가 잡을 수 있는 상태인지 확인
-    //    if (isGrabInNetwork)
-    //    {
-    //        // 2. 잡고 있는 경우라면 해당 잡고 있는 대상이 본인인지 확인
-    //        if (info.Sender.IsLocal
-    //            && isSelect == false)
-    //        {
-    //            // 3. 잡고 있는 사람이 본인이고 놓으려고 시도한다면 놓도록 만들어줌
-
-    //            isGrabInNetwork = false;
-    //        }
-    //        else
-    //        {
-    //            // 4. 잡고 있는 사람이 본인이 아닌데 잡으려고 시도하면 반응 없음 
-    //            Debug.Log("<color=#ecd59c> 이미 누군가 잡고 있습니다. </color>");
-    //        }
-    //    }
-    //    else
-    //    {
-    //        // 5. 아무도 안잡고 있는 경우에 잡으려고 한다면 잡도록 만들어줌
-    //        if (isSelect == true)
-    //        {
-
-    //            isGrabInNetwork = true;
-    //        }
-    //        else
-    //        {
-    //            // 아무도 잡지 않았는데, 해당 물건을 이미 잡고있다가 놓은 상황이라면 오류
-    //            Debug.LogError("<color=red> 잡은 적이 없는 물체를 놓을 수는 없습니다. </color>\"");
-    //        }
-    //    }
-    //}
-
-
 }
