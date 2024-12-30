685e5c09fb137be6d34c890a1ac44272cf532dc4
zxllo12 <poapog123@naver.com>
2024-12-30 21:25:44 +0900

    [Test]테스트 씬 변경

--- Assets/WorkSpaces/PTK/Scripts/PTK_TestGameScene.cs
+++ Assets/WorkSpaces/PTK/Scripts/PTK_TestGameScene.cs
@@ -43,7 +43,7 @@ public class PTK_TestGameScene : MonoBehaviourPunCallbacks
 
     private void PlayerSpawn()
     {
-        Vector3 randomPos = new Vector3(Random.Range(0f, 0f), 0, Random.Range(0, 0f));
-        GameObject player = PhotonNetwork.Instantiate("Player", randomPos, Quaternion.identity);
+        Vector3 randomPos = new Vector3(Random.Range(0f, 0f), 0.2f, Random.Range(0, 0f));
+        GameObject player = PhotonNetwork.Instantiate("PTK_Player", randomPos, Quaternion.identity);
     }
 }
