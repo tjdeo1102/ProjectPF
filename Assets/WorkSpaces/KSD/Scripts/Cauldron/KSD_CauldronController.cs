a4cbdb493b6a80d764f975ddc613a817deadc8f9
tjdeo1102 <57614322+tjdeo1102@users.noreply.github.com>
2025-01-02 18:32:29 +0900

    [Fix] 가마솥 기능 수정
    - 원료와 상호작용 되도록 기능 수정

--- Assets/WorkSpaces/KSD/Scripts/Cauldron/KSD_CauldronController.cs
+++ Assets/WorkSpaces/KSD/Scripts/Cauldron/KSD_CauldronController.cs
@@ -1,16 +1,18 @@
 using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 [RequireComponent(typeof(PhotonView))]
 public class KSD_CauldronController : MonoBehaviourPun
 {
     [Header("기본 설정")]
-    public bool IsFail;         // 실패 여부
     public bool IsFinish;       // 완성 여부
-    public List<KSD_PerfumeNoteInfo> ConcentrateInfos;
+    public List<KSD_PerfumeNoteInfo> ConcentrateInfoList;
+    public List<float> ConcentrateAmountList;
     public KSD_PerfumeNoteInfo ResultNoteInfo;
+    [Range(0f,1f),SerializeField] private float maxFillConcentrateThreshold;     //1회분 원료로 판단할 임계치
 
     [Header("상호작용 설정")]
     [SerializeField] private float finishCookTime;
@@ -25,6 +27,7 @@ public class KSD_CauldronController : MonoBehaviourPun
 
     [Header("참조 설정")]
     [SerializeField] private WGH_FanTest fire;
+    [SerializeField] private KSD_CauldronUI ui;
 
     private float cookTime;
     private float noShakeTime;
@@ -41,7 +44,7 @@ public class KSD_CauldronController : MonoBehaviourPun
         if (AlwaysShake) IsActiveShake = AlwaysShake;
 
         // 불을 켜져 있지 않거나 넣은 재료가 없는 경우는 리턴
-        if (fire.isActiveFire == false || ConcentrateInfos.Count < 1) return;
+        if (fire.isActiveFire == false || ConcentrateInfoList.Count < 1) return;
 
         // 1. 불을 켜져 있는 데, 섞고 있는 경우면 조리 시간 증가
         if (IsActiveShake)
@@ -61,7 +64,6 @@ public class KSD_CauldronController : MonoBehaviourPun
             if (noShakeTime >= noShakeDeadTime)
             {
                 IsFinish = true;
-                IsFail = true;
             }
         }
 
@@ -76,7 +78,9 @@ public class KSD_CauldronController : MonoBehaviourPun
         {
             IsFinish = true;
             // 성공한 경우에 해당 정보를 가진 결과물을 갖고 있어야함.
-            IsFail = !manager.IsValidNoteRecipe(ConcentrateInfos, out ResultNoteInfo);
+            manager.IsValidNoteRecipe(ConcentrateInfoList, out ResultNoteInfo);
+            ConcentrateInfoList.Clear();
+            ConcentrateAmountList.Clear();
         }
     }
     private void NoteFusion()
@@ -87,10 +91,10 @@ public class KSD_CauldronController : MonoBehaviourPun
     public void ResetState()
     {
         // 이전 완료 상태 및 결과 노트 정보 초기화
-        ConcentrateInfos.Clear();
-        ResultNoteInfo = null;
+        ConcentrateInfoList.Clear();
+        ConcentrateAmountList.Clear();
+        ResultNoteInfo = new KSD_PerfumeNoteInfo { Name = PerfumeNoteName.Null, NoteCount = 0, State = PerfumeNoteState.Null};
         IsFinish = false;
-        IsFail = false;
         CurrentPercentage = 0;
 
         // 이전 시간 상태 초기화
@@ -98,21 +102,49 @@ public class KSD_CauldronController : MonoBehaviourPun
         cookTime = 0f;
     }
 
-    private void OnTriggerEnter(Collider other)
+    public void ReceiveConcentrate(KSD_PerfumeNoteInfo con, float getAmount)
     {
-        if (other.CompareTag("Concentrate")
-            && other.TryGetComponent<KSD_LiquidObject>(out var con))
-        {
-            ConcentrateInfos.Add(con.data);
-            PhotonNetwork.Destroy(con.gameObject);
+        // 0. 기존에 노트가 있는 경우에는 재료 추가 안됨
+        if (ResultNoteInfo.Name != PerfumeNoteName.Null) return;
+        // 0-1. 원료의 상태가 잘못되었으면 추가 안됨
+        if (con.State != PerfumeNoteState.Concentrate) return;
 
-            if (ConcentrateInfos.Count > 2)
+        var newCon = new KSD_PerfumeNoteInfo { Name = con.Name, State = con.State, NoteCount = 0};
+
+        // 1. 재료리스트 중에 일치하는 이름이 없으면 새로 추가
+        // 일치하는 아이템이 있는지 확인
+        int index = ConcentrateInfoList.FindIndex(item => item.Name == newCon.Name);
+
+        // 1-1. 재료 리스트의 카운트가 2이상인데 새로운 재료가 추가되면
+        // 꽉차있다고 가정하고 새로운 재료 추가 안되고 리턴
+        if (index == -1)
+        {
+            if (ConcentrateInfoList.Count >= 2) return;
+            // 새로운 재료 추가
+            else
             {
-                IsFail = true;
-                IsFinish = true;
+                index = ConcentrateInfoList.Count;
+                ConcentrateInfoList.Add(newCon);
+                ConcentrateAmountList.Add(0);
             }
         }
-        else if (other.CompareTag("Bucket")
+
+        // 들어온 재료 양 더해주기
+        ConcentrateAmountList[index] += getAmount;
+
+        // 재료가 임계점을 넘은 경우에는 다 찼다고 판정
+        if (ConcentrateAmountList[index] > maxFillConcentrateThreshold)
+        {
+            ConcentrateAmountList[index] = 0f;
+            ConcentrateInfoList[index].NoteCount += 1;
+
+            if (ui != null) ui.SetText($"원료 {ConcentrateInfoList[index].Name.ToString()}가 1회분 추가됨");
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Bucket")
             && other.TryGetComponent<KSD_LiquidObject>(out var note))
         {
             print("양동이에 담음");
@@ -122,6 +154,7 @@ public class KSD_CauldronController : MonoBehaviourPun
         else if(other.CompareTag("Erase"))
         {
             //print("가마솥 리셋");
+            if (PhotonNetwork.IsMasterClient) PhotonNetwork.Destroy(other.gameObject);
             ResetState();
         }
     }
