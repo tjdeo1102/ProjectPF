232bd655f8caa9e485095081f7e0d058121baf79
KimSu-70 <rlatngh4770@naver.com>
2025-01-06 20:51:21 +0900

    [Fix] 슬라이스 관련 기능 수정
    1. 슬라이스 재료 프리팹 수정
    2. 도마 프리팹 구현
    3. 도마 관련 스크립트 생성
    
    #72

---
+++ Assets/WorkSpaces/KSH/Script/Slices/KSH_SlicesCheck.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KSH_SlicesCheck : MonoBehaviour
+{
+    // 슬라이스 가능 재료 체크용
+    [SerializeField] public bool IsSlices = false;
+}
