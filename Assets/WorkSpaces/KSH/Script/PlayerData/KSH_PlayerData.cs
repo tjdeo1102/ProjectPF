0335df02331925448cba5f75ac50b3c667352ee1
KimSu-70 <rlatngh4770@naver.com>
2025-01-10 21:41:52 +0900

    [Feat] 사운드 관련 전체적인 수정
    1. 기존 사운드 매니저 통합 버전으로 수정
    2. 오디오믹스를 추가
    3. 사운드 UI 관련 스크립트 생성

---
+++ Assets/WorkSpaces/KSH/Script/PlayerData/KSH_PlayerData.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KSH_PlayerData
+{
+    public float inputVolume;  // 입력 음량 (0.0 ~ 1.0)
+    public float outputVolume; // 출력 음량 (0.0 ~ 1.0)
+    public float AudioVolume;  // 마스터 음량 (0.0 ~ 1.0)
+    public float BGMVolume;    // 배경음 음량 (0.0 ~ 1.0)
+    public float EffectVolume; // 효과음 음량 (0.0 ~ 1.0)
+}
