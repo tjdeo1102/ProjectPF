0c6b59fef189e07cc13a545ed9dc3babf5c25dd1
leesiyeon99 <qsqs9090@naver.com>
2025-01-16 18:06:19 +0900

    [Fix] 룸에서 혼자 있을시 시작버튼 비활성화

--- Assets/WorkSpaces/LSY/Scripts/MatchMaking/LSY_RoomPanel.cs
+++ Assets/WorkSpaces/LSY/Scripts/MatchMaking/LSY_RoomPanel.cs
@@ -70,7 +70,7 @@ public class LSY_RoomPanel : MonoBehaviour
 
         if (PhotonNetwork.LocalPlayer.IsMasterClient)
         {
-            startButton.interactable = CheckAllReady();
+            startButton.interactable = PhotonNetwork.PlayerList.Length > 1 && CheckAllReady();
         }
         else
         {
