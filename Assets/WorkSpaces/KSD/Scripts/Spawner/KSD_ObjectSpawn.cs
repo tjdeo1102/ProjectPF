09eb4633e9c772976f51f917fcb60fe0c2a7f73d
tjdeo1102 <57614322+tjdeo1102@users.noreply.github.com>
2025-01-17 09:23:54 +0900

    [Fix] 오브젝트 스폰 기능 수정
    - 잡은 오브젝트가 사라지는 현상 수정

--- Assets/WorkSpaces/KSD/Scripts/Spawner/KSD_ObjectSpawn.cs
+++ Assets/WorkSpaces/KSD/Scripts/Spawner/KSD_ObjectSpawn.cs
@@ -61,6 +61,10 @@ public class KSD_ObjectSpawn : MonoBehaviour
             exitObjectRoutine = StartCoroutine(ExitObjectRoutine());
             exitObject = other.gameObject;
             timer = spawnDelay;
+
+            // 맨 뒤로 추가
+            objects.Remove(other.gameObject); 
+            objects.Add(other.gameObject);    
         }
     }
 
@@ -79,13 +83,6 @@ public class KSD_ObjectSpawn : MonoBehaviour
 
     private void SpawnObject()
     {
-        //// 랜덤 위치 계산
-        //Vector3 randomPosition = new Vector3(
-        //    Random.Range(spawnArea.bounds.min.x, spawnArea.bounds.max.x),
-        //    spawnArea.transform.position.y,
-        //    Random.Range(spawnArea.bounds.min.z, spawnArea.bounds.max.z)
-        //);
-
         // 재료 생성
         var obj = PhotonNetwork.Instantiate(objectPath, spawnPosition.position, Quaternion.identity);
         objects.Add(obj);
