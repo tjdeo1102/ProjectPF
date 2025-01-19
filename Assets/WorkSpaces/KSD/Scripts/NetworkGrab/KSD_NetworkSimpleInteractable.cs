7456b4604a122659b4d0dbda554dd6edb42856a4
tjdeo1102 <57614322+tjdeo1102@users.noreply.github.com>
2025-01-20 00:44:32 +0900

    [Fix] 네트워크 Interactable로 기능 교체
    - 레시피 스크린 Interactable교체
    - SimpleInteractable 교체

---
+++ Assets/WorkSpaces/KSD/Scripts/NetworkGrab/KSD_NetworkSimpleInteractable.cs
@@ -0,0 +1,125 @@
+using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+[RequireComponent(typeof(PhotonView))]
+public class KSD_NetworkSimpleInteractable : XRSimpleInteractable
+{
+    [SerializeField] PhotonView photonView;
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        photonView = GetComponent<PhotonView>();
+    }
+
+    protected override void OnSelectEntering(SelectEnterEventArgs args)
+    {
+        // base.OnSelectEntering(args);
+        if (args.interactorObject is XRSocketInteractor)
+        {
+            base.OnSelectEntering(args);
+        }
+        else
+        {
+            PhotonView interactorPV = args.interactorObject.transform.GetComponent<PhotonView>();
+            photonView.RPC(nameof(RPC_SelectEntering), RpcTarget.AllViaServer, interactorPV.ViewID);
+        }
+    }
+
+    [PunRPC]
+    public void RPC_SelectEntering(int interactorID)
+    {
+        SelectEnterEventArgs args = new SelectEnterEventArgs();
+        args.interactorObject = PhotonView.Find(interactorID).GetComponent<IXRSelectInteractor>();
+        args.interactableObject = this;
+        args.manager = interactionManager;
+
+        base.OnSelectEntering(args);
+        Debug.Log($"{photonView.Owner} {gameObject.name} Select Entering -> {args.interactorObject.transform.gameObject.name}");
+    }
+
+    protected override void OnSelectEntered(SelectEnterEventArgs args)
+    {
+        // base.OnSelectEntered(args);
+        if (args.interactorObject is XRSocketInteractor)
+        {
+            base.OnSelectEntered(args);
+        }
+        else
+        {
+            PhotonView interactorPV = args.interactorObject.transform.GetComponent<PhotonView>();
+            photonView.RPC(nameof(RPC_SelectEntered), RpcTarget.AllViaServer, interactorPV.ViewID);
+        }
+    }
+
+    [PunRPC]
+    public void RPC_SelectEntered(int interactorID)
+    {
+        SelectEnterEventArgs args = new SelectEnterEventArgs();
+        args.interactorObject = PhotonView.Find(interactorID).GetComponent<IXRSelectInteractor>();
+        args.interactableObject = this;
+        args.manager = interactionManager;
+
+        base.OnSelectEntered(args);
+        Debug.Log($"{photonView.Owner} {gameObject.name} Select Entered -> {args.interactorObject.transform.gameObject.name}");
+    }
+
+    protected override void OnSelectExiting(SelectExitEventArgs args)
+    {
+        // base.OnSelectExiting(args);
+        if (args.interactorObject is XRSocketInteractor)
+        {
+            base.OnSelectExiting(args);
+        }
+        else
+        {
+            PhotonView interactorPV = args.interactorObject.transform.GetComponent<PhotonView>();
+            photonView.RPC(nameof(RPC_SelectExiting), RpcTarget.AllViaServer, interactorPV.ViewID, args.isCanceled);
+        }
+    }
+
+    [PunRPC]
+    public void RPC_SelectExiting(int interactorID, bool isCanceled)
+    {
+        SelectExitEventArgs args = new SelectExitEventArgs();
+        args.interactorObject = PhotonView.Find(interactorID).GetComponent<IXRSelectInteractor>();
+        args.interactableObject = this;
+        args.manager = interactionManager;
+        args.isCanceled = isCanceled;
+
+        base.OnSelectExiting(args);
+        Debug.Log($"{photonView.Owner} {gameObject.name} Select Exiting -> {args.interactorObject.transform.gameObject.name}");
+    }
+
+    protected override void OnSelectExited(SelectExitEventArgs args)
+    {
+        // base.OnSelectExited(args);
+
+        if (args.interactorObject is XRSocketInteractor)
+        {
+            base.OnSelectExited(args);
+        }
+        else
+        {
+            PhotonView interactorPV = args.interactorObject.transform.GetComponent<PhotonView>();
+            photonView.RPC(nameof(RPC_SelectExited), RpcTarget.AllViaServer, interactorPV.ViewID, args.isCanceled);
+        }
+    }
+
+    [PunRPC]
+    public void RPC_SelectExited(int interactorID, bool isCanceled)
+    {
+        SelectExitEventArgs args = new SelectExitEventArgs();
+        args.interactorObject = PhotonView.Find(interactorID).GetComponent<IXRSelectInteractor>();
+        args.interactableObject = this;
+        args.manager = interactionManager;
+        args.isCanceled = isCanceled;
+
+        base.OnSelectExited(args);
+        Debug.Log($"{photonView.Owner} {gameObject.name} Select Exited -> {args.interactorObject.transform.gameObject.name}");
+    }
+}
